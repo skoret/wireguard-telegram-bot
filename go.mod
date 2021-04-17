a073c172897ea491b1bf1c22a794f2a9d0ca0ff5
Sergey Skaredov <sergey.skoret@gmail.com>
2021-04-18 00:19:52 +0300

    Setup NoOp telegram bot

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/skoret/wireguard-bot
+
+go 1.16
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.0.0-rc1.0.20210311030851-d0e1dfd8c604 // indirect
---
+++ wg-tg.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true
+	self, err := bot.GetMe()
+	fmt.Printf("%+v\n", self)
+}
