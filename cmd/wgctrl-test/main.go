f1f0f26ad3da05e2ea89f32ebc48d149efc05a32
Sergey Skaredov <sergey.skoret@gmail.com>
2021-04-27 19:11:39 +0300

    Experiments with wgctrl-go

---
+++ cmd/wgctrl-test/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"golang.zx2c4.com/wireguard/wgctrl"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	flag.Parse()
+
+	c, err := wgctrl.New()
+	if err != nil {
+		log.Fatalf("failed to open wgctrl: %v", err)
+	}
+	defer c.Close()
+
+	device, err := c.Device("wg0")
+	if err != nil {
+		log.Fatalf("failed to get device %q: %v", device, err)
+	}
+	show(device)
+
+	fmt.Println("---- add new peer ----")
+	pri, err := wgtypes.GeneratePrivateKey()
+	if err != nil {
+		log.Fatalf("failed to generate private key: %v", err)
+	}
+	pub := pri.PublicKey()
+	_, ipNet, err := net.ParseCIDR("10.8.0.3/32")
+	if err != nil {
+		log.Fatalf("failed to parse ip with mask: %v", err)
+	}
+	log.Printf("private key: %s", pri.String())
+	log.Printf("public  key: %s", pub.String())
+
+	cfg := wgtypes.Config{
+		ReplacePeers: false,
+		Peers: []wgtypes.PeerConfig{
+			{
+				PublicKey:                   pub,
+				Remove:                      false,
+				UpdateOnly:                  false,
+				PresharedKey:                nil,
+				Endpoint:                    nil,
+				PersistentKeepaliveInterval: nil,
+				ReplaceAllowedIPs:           false,
+				AllowedIPs:                  []net.IPNet{*ipNet},
+			},
+		},
+	}
+
+	if err := c.ConfigureDevice("wg0", cfg); err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println(err)
+		} else {
+			log.Fatalf("Unknown config error: %v", err)
+		}
+	}
+
+	show(device)
+
+	fmt.Println("we need to run 'wg-quick save wg0' to dump updated interface config to config file")
+}
+
+func show(d *wgtypes.Device) {
+	fmt.Println("---- wg device ----")
+	printDevice(d)
+	for _, p := range d.Peers {
+		printPeer(p)
+	}
+	fmt.Println("-------------------")
+}
+
+func printDevice(d *wgtypes.Device) {
+	const f = `interface: %s (%s)
+  public key: %s
+  private key: (hidden)
+  listening port: %d
+
+`
+
+	fmt.Printf(
+		f,
+		d.Name,
+		d.Type.String(),
+		d.PublicKey.String(),
+		d.ListenPort)
+}
+
+func printPeer(p wgtypes.Peer) {
+	const f = `peer: %s
+  endpoint: %s
+  allowed ips: %s
+  latest handshake: %s
+  transfer: %d B received, %d B sent
+
+`
+
+	fmt.Printf(
+		f,
+		p.PublicKey.String(),
+		// TODO(mdlayher): get right endpoint with getnameinfo.
+		p.Endpoint.String(),
+		ipsString(p.AllowedIPs),
+		p.LastHandshakeTime.String(),
+		p.ReceiveBytes,
+		p.TransmitBytes,
+	)
+}
+
+func ipsString(ipns []net.IPNet) string {
+	ss := make([]string, 0, len(ipns))
+	for _, ipn := range ipns {
+		ss = append(ss, ipn.String())
+	}
+
+	return strings.Join(ss, ", ")
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.0.0-rc1.0.20210311030851-d0e1dfd8c604
 	github.com/joho/godotenv v1.3.0
+	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20200609130330-bd2cb7843e1b
 )
