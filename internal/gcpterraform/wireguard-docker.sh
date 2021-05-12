616ef05c1a14fbd9b8cc06c3166dd73b200c4ba4
Sergey Skaredov <sergey.skoret@gmail.com>
2021-05-12 23:54:43 +0300

    Fix wireguard container volumes

--- internal/gcpterraform/wireguard-docker.sh
+++ internal/gcpterraform/wireguard-docker.sh
@@ -14,7 +14,7 @@ docker run -d \
   -p 51820:51820/udp \
   -e PEERS=1 \
   -e PEERDNS=auto \
-  -v /etc/wireguard/wg0.conf:/etc/wireguard/wg0.conf:rw \
+  -v /etc/wireguard:/config \
   -v /lib/modules:/lib/modules \
   --restart unless-stopped \
   ghcr.io/linuxserver/wireguard
