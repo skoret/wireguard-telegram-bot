a5afcb4965926028a2950ee63a979222f89ce893
Sergey Skaredov <sergey.skoret@gmail.com>
2021-05-13 02:09:52 +0300

    Implement dynamic ip net selection

--- internal/wireguard/wireguard.go
+++ internal/wireguard/wireguard.go
@@ -1,6 +1,7 @@
 package wireguard
 
 import (
+	"bytes"
 	"io"
 	"log"
 	"net"
@@ -52,18 +53,21 @@ func (w *Wireguard) CreateNewConfig() (io.Reader, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to generate private key")
 	}
-	// TODO: get ip net dynamically
 	ipNet, err := w.getNextIPNet()
 	if err != nil {
 		return nil, err
 	}
 
+	device, err := w.client.Device(w.device)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get device "+w.device)
+	}
 	clientConfig := cfgs.ClientConfig{
 		Address:    ipNet.String(),
 		PrivateKey: pri.String(),
 		DNS:        w.dns,
 
-		PublicKey:  os.Getenv("SERVER_PUB_KEY"),
+		PublicKey:  device.PublicKey.String(),
 		AllowedIPs: []string{"0.0.0.0/0"},
 		Endpoint:   os.Getenv("SERVER_ENDPOINT"),
 	}
@@ -88,16 +92,6 @@ func (w *Wireguard) CreateNewConfig() (io.Reader, error) {
 	return cfgFile, nil
 }
 
-// TODO: get ip net dynamically
-func (w *Wireguard) getNextIPNet() (*net.IPNet, error) {
-	address := "10.8.0.3/32"
-	_, ipNet, err := net.ParseCIDR(address)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to parse ip with mask")
-	}
-	return ipNet, nil
-}
-
 func (w *Wireguard) updateDevice(cfg wgtypes.Config) error {
 	if err := w.client.ConfigureDevice(w.device, cfg); err != nil {
 		return errors.Wrap(err, "failed to update server configuration")
@@ -108,4 +102,67 @@ func (w *Wireguard) updateDevice(cfg wgtypes.Config) error {
 		return errors.Wrap(err, "failed to dump server config to conf file")
 	}
 	return nil
+}
+
+func (w *Wireguard) getNextIPNet() (*net.IPNet, error) {
+	ip, err := w.getLatestUsedIP()
+	if err != nil {
+		return nil, err
+	}
+
+	return &net.IPNet{
+		IP:   nextIP(ip, 1),
+		Mask: net.IPv4Mask(255, 255, 255, 255),
+	}, nil
+}
+
+// Thanks to https://gist.github.com/udhos/b468fbfd376aa0b655b6b0c539a88c03
+func nextIP(ip net.IP, inc uint) net.IP {
+	i := ip.To4()
+	v := uint(i[0])<<24 + uint(i[1])<<16 + uint(i[2])<<8 + uint(i[3])
+	v += inc
+	v3 := byte(v & 0xFF)
+	v2 := byte((v >> 8) & 0xFF)
+	v1 := byte((v >> 16) & 0xFF)
+	v0 := byte((v >> 24) & 0xFF)
+	return net.IPv4(v0, v1, v2, v3)
+}
+
+func (w *Wireguard) getLatestUsedIP() (net.IP, error) {
+	device, err := w.client.Device(w.device)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get device "+w.device)
+	}
+	lastIP, err := w.getDeviceAddress()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get address for device "+w.device)
+	}
+	for _, peer := range device.Peers {
+		for _, ipNet := range peer.AllowedIPs {
+			if bytes.Compare(ipNet.IP, lastIP) >= 0 {
+				lastIP = ipNet.IP
+			}
+		}
+	}
+	if lastIP == nil {
+		return nil, errors.New("failed to get latest used ip for device " + w.device)
+	}
+	return lastIP, nil
+}
+
+func (w *Wireguard) getDeviceAddress() (net.IP, error) {
+	ife, err := net.InterfaceByName(w.device)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get interface "+w.device)
+	}
+	addrs, err := ife.Addrs()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get address for interface "+w.device)
+	}
+	for _, addr := range addrs {
+		if ipv4Addr := addr.(*net.IPNet).IP.To4(); ipv4Addr != nil {
+			return ipv4Addr, nil
+		}
+	}
+	return nil, errors.New("failed to get address for interface " + w.device)
 }
